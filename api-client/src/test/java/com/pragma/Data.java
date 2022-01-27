eae0020c9cd6b4b8acb45683c8137e4887265433
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-27 17:52:49 -0500

    Add test find all

--- api-client/src/test/java/com/pragma/Data.java
+++ api-client/src/test/java/com/pragma/Data.java
@@ -1,8 +1,15 @@
 package com.pragma;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.pragma.model.Client;
 
 public class Data {
 	public static final Client CLIENT = new Client(1L, "Sergio Stives", "Barrios Buitrago", "CC", 1010058716L, 21,
 			"Cali");
+
+	public static final List<Client> LIST_CLIENTS = Arrays.asList(CLIENT,
+			new Client(1L, "Jhonatan Javier", "Barrios Buitrago", "CC", 10100232L, 22, "Bucaramanga"),
+			new Client(2L, "Claudia", "Buitrago", "CC", 60358816L, 52, "Cucuta"));
 }
--- api-client/src/test/java/com/pragma/service/impl/ClientServiceImplTest.java
+++ api-client/src/test/java/com/pragma/service/impl/ClientServiceImplTest.java
@@ -1,11 +1,11 @@
 package com.pragma.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -17,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.pragma.model.Client;
 
 import static com.pragma.Data.CLIENT;
@@ -27,13 +28,17 @@ public class ClientServiceImplTest {
 	@Mock
 	DynamoDBMapper mapper;
 	
+	@Mock
+	DynamoDBScanExpression scan;
+	
 	@InjectMocks
 	ClientServiceImpl service;
 
 	@Tag("get")
+	@Tag("find")
 	@Nested
-	@DisplayName("Clase que permite consultar por el id.")
-	class ClientServiceImplTestFindById{
+	@DisplayName("Clase que permite consultar por un condicion y trae un resultado.")
+	class ClientServiceImplTestFindBy{
 		
 		@Test
 		@DisplayName("Consultar por el id.")
@@ -43,8 +48,24 @@ public class ClientServiceImplTest {
 			Client client = service.findById(1L);
 			
 			assertEquals(CLIENT.getId(), client.getId());
+			assertEquals(CLIENT, client);
 			
 			verify(mapper, timeout(1)).load(Client.class, 1L);
+		}
+	}
+	
+	@Tag("get")
+	@Tag("findAll")
+	@Nested
+	@DisplayName("lase que permite consultar por un condicion y trae varios resultados.")
+	class ClientServiceImplTestFindAll{
+		
+		@Test
+		@DisplayName("Consultar por todos los usuarios.")
+		void findAll() {
+			when(mapper.scan(Client.class, scan)).thenReturn(null);
+			
+			verify(mapper, timeout(1)).scan(Client.class, scan);
 		}
 	}
 }
