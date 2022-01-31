490e0ca5cd886aef9399b6440c3224b49a718788
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-31 09:36:22 -0500

    Add stream data

--- api-client/src/test/java/com/pragma/Data.java
+++ api-client/src/test/java/com/pragma/Data.java
@@ -2,24 +2,28 @@ package com.pragma;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.pragma.model.dto.ClientDTO;
 import com.pragma.model.entity.Client;
 import com.pragma.request.ClientRequest;
 
 public class Data {
-	
+
 	public static final ClientDTO CLIENT = new ClientDTO(1L, "Sergio Stives", "Barrios Buitrago", "CC", 1010058716L, 21,
 			"Cali");
-	public static final Client CLIENT_ENTITY = new Client(1L, "Sergio Stives", "Barrios Buitrago", "CC", 1010058716L, 21,
-			"Cali");
 	
+	public static final Client CLIENT_ENTITY = new Client(CLIENT.getId(), CLIENT.getName(), CLIENT.getSubname(),
+			CLIENT.getType(), CLIENT.getDocument(), CLIENT.getAge(), CLIENT.getCityBirth());
+
 	public static final ClientRequest CLIENT_REQUEST = new ClientRequest(1L, "GET", CLIENT);
 
 	public static final List<ClientDTO> LIST_CLIENTS = Arrays.asList(CLIENT,
 			new ClientDTO(1L, "Jhonatan Javier", "Barrios Buitrago", "CC", 10100232L, 22, "Bucaramanga"),
 			new ClientDTO(2L, "Claudia", "Buitrago", "CC", 60358816L, 52, "Cucuta"));
-	public static final List<Client> LIST_CLIENTS_ENTITY = Arrays.asList(CLIENT_ENTITY,
-			new Client(1L, "Jhonatan Javier", "Barrios Buitrago", "CC", 10100232L, 22, "Bucaramanga"),
-			new Client(2L, "Claudia", "Buitrago", "CC", 60358816L, 52, "Cucuta"));
+	
+	public static final List<Client> LIST_CLIENTS_ENTITY = LIST_CLIENTS.stream()
+			.map(client -> new Client(client.getId(), client.getName(), client.getSubname(), client.getType(),
+					client.getDocument(), client.getAge(), client.getCityBirth()))
+			.collect(Collectors.toList());
 }
